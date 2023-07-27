5c534d77fbc8f0e4fd664a05d1954de84aa2d77e
Asad1508 <109195461+Asad1508@users.noreply.github.com>
2023-07-27 09:27:38 +0500

    Update intro.js

--- src/intro.js
+++ src/intro.js
@@ -3,15 +3,8 @@ import React from "react";
 const Intro = () => {
   return (
     <div id="intro">
-      <h1 class="title"> The Hexaa ReactJS Code Test</h1>
       <h1 class="subtitle"> Q1: PhoneBook</h1>
       <div class="description">
-        <p>Your task is to implement a simple phonebook with functionality. </p>
-        <ul>
-          <li>Contact should be added from the contact form</li>
-          <li>The contacts should be displayed in alphabetical order</li>
-          <li>The contact should be deleted by pressing the delete icon</li>
-        </ul>
       </div>
     </div>
   );
